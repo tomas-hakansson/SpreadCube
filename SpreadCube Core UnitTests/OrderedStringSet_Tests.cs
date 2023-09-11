31fd96343a5f6b0869da301bf53bb737e61d15ab
Tomas Håkansson <tomas@pennybridge.org>
2023-09-11 19:47:36 +0200

    Added a new type with tests that acts like a hybrid between a list and a set, that should allow me to add items at exact locations

---
+++ SpreadCube Core UnitTests/OrderedStringSet_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadCube_Core;
+
+namespace SpreadCube_Core_UnitTests;
+
+[TestClass]
+public class OrderedStringSet_Tests
+{
+    [TestMethod]
+    public void Add_addingTheSameValueTwice_doesNotChangeTheList()
+    {
+        OrderedStringSet oss1 = new()
+        {
+            "one",
+            "two",
+            "three"
+        };
+
+        OrderedStringSet oss2 = new()
+        {
+            "one",
+            "two",
+            "three"
+        };
+
+        CollectionAssert.AreEqual(oss1, oss2);
+        oss1.Add("two");
+        CollectionAssert.AreEqual(oss1, oss2);
+    }
+
+    [TestMethod]
+    public void CanGetFirst()
+    {
+        OrderedStringSet oss = new()
+        {
+            "one",
+            "two",
+            "three"
+        };
+
+        Assert.AreEqual("one", oss.First());
+    }
+}
---
+++ SpreadCube Core/OrderedStringSet.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+
+namespace SpreadCube_Core;
+
+public class OrderedStringSet : IList, IList<NonEmptyString>
+{
+    LinkedList<NonEmptyString> _list;
+
+    public OrderedStringSet()
+    {
+        _list = new LinkedList<NonEmptyString>();
+    }
+
+    public NonEmptyString this[int index]
+    {
+        get => GetByIndex(index);
+        set => throw new NotImplementedException();
+    }
+
+    NonEmptyString GetByIndex(int index)
+    {
+        if (index < 0)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index starts at 0 (zero).");
+        if (index >= _list.Count)
+            throw new ArgumentOutOfRangeException(nameof(index), "Index must not exceed collection.Count - 1.");
+
+        LinkedListNode<NonEmptyString> currentNode = new("placeholder");
+
+        for (int i = 0; i <= index; i++)
+        {
+            if (i == 0)
+            {
+                if (_list.First == null)
+                    throw new NullReferenceException("First value of collection is null.");
+
+                currentNode = _list.First;
+            }
+            else
+            {
+                if (currentNode.Next == null)
+                    throw new NullReferenceException("Next value of collection is null.");
+
+                currentNode = currentNode.Next;
+            }
+        }
+
+        return currentNode.Value;
+    }
+
+    public int Count => _list.Count;
+
+    public void Add(NonEmptyString item)
+    {
+        if (!Contains(item))
+            _list.AddLast(item);
+    }
+
+    public bool Contains(NonEmptyString item) =>
+        _list.Contains(item);
+
+    IEnumerator IEnumerable.GetEnumerator() =>
+        _list.GetEnumerator();
+
+
+    //Inherited methods, unimplemented because they're unused:
+
+    object? IList.this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    bool IList.IsReadOnly => throw new NotImplementedException();
+    bool ICollection<NonEmptyString>.IsReadOnly => throw new NotImplementedException();
+    bool IList.IsFixedSize => throw new NotImplementedException();
+    bool ICollection.IsSynchronized => throw new NotImplementedException();
+    object ICollection.SyncRoot => throw new NotImplementedException();
+    public int Add(object? value) => throw new NotImplementedException();
+    public void Clear() => throw new NotImplementedException();
+    public bool Contains(object? value) => throw new NotImplementedException();
+    public void CopyTo(NonEmptyString[] array, int arrayIndex) => throw new NotImplementedException();
+    public void CopyTo(Array array, int index) => throw new NotImplementedException();
+    public IEnumerator<NonEmptyString> GetEnumerator() => throw new NotImplementedException();
+    public int IndexOf(NonEmptyString item) => throw new NotImplementedException();
+    public int IndexOf(object? value) => throw new NotImplementedException();
+    public void Insert(int index, NonEmptyString item) => throw new NotImplementedException();
+    public void Insert(int index, object? value) => throw new NotImplementedException();
+    public bool Remove(NonEmptyString item) => throw new NotImplementedException();
+    public void Remove(object? value) => throw new NotImplementedException();
+    public void RemoveAt(int index) => throw new NotImplementedException();
+}
